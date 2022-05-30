f8e92cce9f776ccf56e560fd1ab064752213434b
Kyle Awayan <kyle@awayan.com>
2022-05-30 14:48:49 -0700

    create spotify auth url

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import indexRouter from "./routes/indexRouter";
 import apiRouter from "./routes/apiRouter";
+import spotifyAuthRouter from "./routes/spotifyAuthRouter";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
+app.use("/auth", spotifyAuthRouter);
 
 app.listen(port, () => {
   console.log(`Senre API listening on port ${port}`);
